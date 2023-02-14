65ba10c59996cde8dfb262fd980c8cccbffcf8a8
Gabriel Guaitanele <gabrieljmym13@gmail.com>
2023-02-14 16:42:23 -0300

    configs de urls de produção

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,9 @@
 export const environment = {
     production: true,
-
     API_URL: 'https://song-guesser-api-production.up.railway.app',
     OAUTH_CALLBACK_URL: 'https://song-guesser.vercel.app/authorized',
     LOGOUT_REDIRECT_TO_URL: 'https://song-guesser.vercel.app/login',
     TOKEN_ALLOWED_DOMAINS: [ /song-guesser-api-production.up.railway.app/ ],
     TOKEN_DISALLOWED_DOMAINS: [ /\/oauth2\/token/ ],
     CLIENT_CREDENTIALS: 'Basic ' + process.env['CLIENT_CREDENTIALS']
-
-    
 }
