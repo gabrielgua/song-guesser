fb6e6c920703992f5e791cf8d6de8544f810bb5a
Gabriel Guaitanele <gabrieljmym13@gmail.com>
2023-02-14 16:08:10 -0300

    enviroment prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,12 @@
 export const environment = {
     production: true,
 
+    API_URL: 'https://song-guesser-api-production.up.railway.app',
+    OAUTH_CALLBACK_URL: 'http://127.0.0.1:4200/authorized',
+    LOGOUT_REDIRECT_TO_URL: 'http://127.0.0.1:4200/login',
+    TOKEN_ALLOWED_DOMAINS: [ /song-guesser-api-production.up.railway.app/ ],
+    TOKEN_DISALLOWED_DOMAINS: [ /\/oauth2\/token/ ],
+    CLIENT_CREDENTIALS: 'Basic ' + process.env['CLIENT_CREDENTIALS']
+
     
 }
