c869637cf6b9325b9706fa84355bbd1859227e62
Gabriel Guaitanele <gabrieljmym13@gmail.com>
2023-02-17 17:57:42 -0300

    alterando url para aws

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
     production: true,
-    API_URL: 'https://song-guesser-api-production.up.railway.app',
+    API_URL: 'http://song-guesser-api-dev.us-west-2.elasticbeanstalk.com',
     OAUTH_CALLBACK_URL: 'https://song-guesser.vercel.app/authorized',
     LOGOUT_REDIRECT_TO_URL: 'https://song-guesser.vercel.app/login',
-    TOKEN_ALLOWED_DOMAINS: [ /song-guesser-api-production.up.railway.app/ ],
+    TOKEN_ALLOWED_DOMAINS: [ /song-guesser-api-dev.us-west-2.elasticbeanstalk.com/ ],
     TOKEN_DISALLOWED_DOMAINS: [ /\/oauth2\/token/ ],
     CLIENT_CREDENTIALS: 'Basic ' + process.env['CLIENT_CREDENTIALS']
 }
